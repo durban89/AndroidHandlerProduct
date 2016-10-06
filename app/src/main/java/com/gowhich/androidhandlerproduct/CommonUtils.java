af4a22cd33b79528c60d2fc61f5aefb83d24e159
dapeng126 <zhangdapeng89@126.com>
2016-10-06 23:35:21 +0800

    配置服务器数据访问路径

---
+++ app/src/main/java/com/gowhich/androidhandlerproduct/CommonUtils.java
@@ -0,0 +1,10 @@
+package com.gowhich.androidhandlerproduct;
+
+/**
+ * Created by durban126 on 16/10/6.
+ */
+
+public class CommonUtils {
+    public static final String PRODUCT_URL = "http://192.168.31.220:8080/ProductManagement/servlet/JsonAction?action_flag=more";
+    public static final String IMAGE_URL = "http://192.168.31.220:8080/ProductManagement/upload/";
+}
